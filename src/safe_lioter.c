5023806aaab16bfa23016a9a1001f7a4e1e368ec
jazzazi007 <maaljazzazi22@eng.just.edu.jo>
2025-06-20 23:58:09 +0300

    safe altitude rather than the radius

--- src/safe_lioter.c
+++ src/safe_lioter.c
@@ -20,5 +20,26 @@ void safe_lioter(sts *sts)
             else
                 turn+=1;
     }
+}
 
+void safe_alt(sts *sts)
+{
+    int init_alt = 100;
+    int distance = 10;
+    int alt_0 = (UAV_COUNT-1)/2;
+    sts->stable_alt[0][0] = 100 + (distance * alt_0);
+    int turn = 0;
+    bool check = false;
+    for (int i = 1; i < UAV_COUNT; i++)
+    {
+            sts->stable_alt[i][0] = init_alt + (distance * turn);
+            if (i+1 > UAV_COUNT/2 && !check)
+            {
+                check = true;
+                turn+=2;
+            }
+              
+            else
+                turn+=1;
+    }
 }
